d17dba5f896c4e90dbd5c2540c128ffef4656b2e
Emanuel120106 <skuanchi1201@gmail.com>
2025-12-05 10:31:10 -0500

    Add files via upload

---
+++ mi-plugin-git.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Mi Plugin Git
+ * Description: Plugin de prueba para actualización vía GitHub usando Git Updater.
+ * Version: 1.0.5
+ * Author: Emanuel Iglesias
+ * GitHub Plugin URI: https://github.com/Emanuel120106/MiPluginGit
+ * GitHub Branch: main
+ */
