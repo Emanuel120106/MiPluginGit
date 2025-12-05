68c605909ea2cfcec8be8415a609323d6df4ccff
Emanuel120106 <skuanchi1201@gmail.com>
2025-12-05 18:26:09 -0500

    Add files via upload

---
+++ v4.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Performance Lab
+ * Plugin URI: https://github.com/WordPress/performance
+ * Description: Performance plugin from the WordPress Performance Team, which is a collection of standalone performance features.
+ * Requires at least: 6.6
+ * Requires PHP: 7.2
+ * Version: 4.0.0
+ * Author: WordPress Performance Team
+ * Author URI: https://make.wordpress.org/performance/
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: performance-lab
+ *
+ * @package performance-lab
+ */
