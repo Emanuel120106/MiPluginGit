9e06376f4e00d6be647b7728d6fd855fdd5ca899
Emanuel120106 <skuanchi1201@gmail.com>
2025-12-05 10:40:02 -0500

    mi-plugin-git.php

--- mi-plugin-git.php
+++ mi-plugin-git.php
@@ -2,9 +2,10 @@
 /**
  * Plugin Name: Mi Plugin Git
  * Description: Plugin de prueba para actualización vía GitHub usando Git Updater.
- * Version: 1.0.6
+ * Version: 1.0.7
  * Author: Emanuel Iglesias
  * GitHub Plugin URI: https://github.com/Emanuel120106/MiPluginGit
  * GitHub Branch: main
  */
+
 
